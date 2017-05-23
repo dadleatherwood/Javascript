e960ddfecbd2ef1afc0e055adc4819752a83f849
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-05-23 08:27:58 -0500

    change to removeVowels file

--- removeVowels.js
+++ removeVowels.js
@@ -1,12 +1,15 @@
 function removeVowels (str){
-  arr = str.toLowerCase().split('');
-  sentence = [];
+  var arr = str.toLowerCase().split('');
   for ( var i = 0; i < arr.length; i++){
     if((arr[i] === 'a') || (arr[i] === 'e') || (arr[i] === 'i') || (arr[i] === 'o') || (arr[i] === 'u')) {
       arr.splice(i,1);
     }
   }
   return arr.join('')
+}
+
+function removeVowels(str) {
+  return str.replace(/[aeiou]/gi, '') //quicker way
 }
 
 removeVowels("This website is for losers LOL!");
